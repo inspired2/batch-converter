4fa0aba80d119eb46774adf1363113272f783889
= <=>
2021-09-28 22:21:56 +1000

    tiny refactoring

--- src/bin.rs
+++ src/bin.rs
@@ -11,9 +11,9 @@ fn main() -> Result<(), CustomError>{
     let mut runtime_args = std::env::args();
     let args_length = runtime_args.len();
     //first arg is always path-to-executable:
-    let execution_path = runtime_args.next().unwrap();
+    let executable_path = runtime_args.next().unwrap();
 
-    let execution_dir= trim_filename(&execution_path);
+    let execution_dir= trim_filename(&executable _path);
     //if argument provided it is considered to be absolute path to the target directory
     if args_length > 1 {process_all(runtime_args.last().unwrap())?;}
     else {
